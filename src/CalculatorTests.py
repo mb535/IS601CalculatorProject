98f2c6d1ffe720459e81179ce7e06145cd6b6293
mb535 <mb535@njit.edu>
2020-11-01 01:02:08 -0400

    Feature: addition test complete and working

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,12 +1,23 @@
 import unittest
-
 from Calculator import Calculator
-
+from CsvReader import CsvReader
+from pprint import pprint
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
+
+
+    def test_addition(self):
+        test_data = CsvReader('/src/csvfiles/Unit Test Addition.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
 if __name__ == '__main__':
     unittest.main()
