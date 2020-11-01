69cd3365779fb94110918691f0328d3ab3277ea6
mb535 <mb535@njit.edu>
2020-11-01 00:34:13 -0400

    Task: finished defining functions on Calculator.py

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,6 +1,60 @@
+from CsvReader import CsvReader
+
+def addition(a, b):
+    c = a + b
+    return c
+
+
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+def multiplication(a, b):
+    c = a*b
+    return c
+
+def division(a, b):
+    c = a/b
+    return c
+
+def square(a):
+    c = a*a
+    return c
+
+def square_root(a):
+    c = a**(.5)
+    return c
+
+
 
 class Calculator:
     result = 0
 
     def __init__(self):
+        x = 2 + 2
+        self.result = x;
         pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = division(a, b)
+
+    def square(self, a):
+        self.result = square(a)
+        return self.result
+
+    def square_root(self, a):
+        self.result = square_root(a)
